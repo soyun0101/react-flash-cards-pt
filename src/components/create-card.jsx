c9bcce995d07a9e51641b3f461b804fb0c01f226
Sophia <sophiayun@yahoo.com>
2020-02-26 21:54:27 -0800

    added addCard saveCard and passed Props to create card

--- src/components/create-card.jsx
+++ src/components/create-card.jsx
@@ -2,8 +2,70 @@ import React from 'react';
 
 
 class CreateCard extends React.Component{
-    render(){
-        return <h1>Create New Card</h1>
+    constructor(props){
+        super(props);
+
+        this.state = {
+            question: '',
+            answer: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(e){
+
+        const target= e.target;
+
+        console.log(target.value);
+
+        if(target.name === 'question'){
+            this.setState({
+                question: target.value
+            });
+        }
+        if(target.name === 'answer'){
+            this.setState({
+                answer: target.value
+            });
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+        this.props.addCard({
+            question: this.state.question,
+            answer: this.state.answer
+        });
+    }
+
+    render(){
+
+        console.log('q: ', this.state.question);
+        console.log('a: ', this.state.answer);
+        return <form onSubmit={this.handleSubmit}>
+        <h1>Create New Card</h1>
+        <label htmlFor="question">
+            Question:
+            <div>
+                <textarea onChange={this.handleChange} name="question" id="question" cols="70" rows="2"></textarea>
+            </div>    
+        </label>
+        <label htmlFor="answer">
+            Answer:
+            <div>
+                <textarea onChange={this.handleChange} name="answer" id="answer" cols="70" rows="2"></textarea>
+            </div>   
+        </label>
+        <button>Cancel</button>
+        <button type="submit">Save Card</button>
+    </form>
+    }
+
+
+
+
 }
 export default CreateCard;
